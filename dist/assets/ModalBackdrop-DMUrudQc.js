3aaca5de0d56e82ee30ac53b6fafa26a09bf955e
retcherrakha-max <retcherrakha@gmail.com>
2026-09-12 15:51:56 -0700

    Add files via upload

---
+++ dist/assets/ModalBackdrop-DMUrudQc.js
@@ -0,0 +1,11 @@
+import{c as t}from"./index-CTlSZp8S.js";import{r as c,j as p}from"./react-D1AEjouf.js";/**
+ * @license lucide-react v0.294.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const u=t("AlertTriangle",[["path",{d:"m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z",key:"c3ski4"}],["path",{d:"M12 9v4",key:"juzpu7"}],["path",{d:"M12 17h.01",key:"p32p05"}]]);/**
+ * @license lucide-react v0.294.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const k=t("Code2",[["path",{d:"m18 16 4-4-4-4",key:"1inbqp"}],["path",{d:"m6 8-4 4 4 4",key:"15zrgr"}],["path",{d:"m14.5 4-5 16",key:"e7oirm"}]]);function m({className:a,onDismiss:o,children:n,...s}){const e=c.useRef(!1);return p.jsx("div",{className:a,onMouseDown:r=>{e.current=r.target===r.currentTarget},onClick:r=>{e.current&&r.target===r.currentTarget&&o?.(),e.current=!1},...s,children:n})}export{u as A,k as C,m as M};
